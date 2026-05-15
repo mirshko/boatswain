3812314cc2417828ef3ec724d0800aa7e4821618
Jeff Reiner <8116716+mirshko@users.noreply.github.com>
2026-05-15 17:47:01 +0200

    Revert to standard About panel, remove custom AboutView

--- Boatswain/Views/MoreMenu.swift
+++ Boatswain/Views/MoreMenu.swift
@@ -12,14 +12,8 @@ struct MoreMenu: View {
 
     var body: some View {
         Button("About") {
-            let aboutView = AboutView()
-            let hostingController = NSHostingController(rootView: aboutView)
-            let window = NSWindow(contentViewController: hostingController)
-            window.title = "About Boatswain"
-            window.styleMask = [.titled, .closable]
-            window.setContentSize(NSSize(width: 320, height: 260))
-            window.makeKeyAndOrderFront(nil)
             NSApp.activate(ignoringOtherApps: true)
+            NSApp.orderFrontStandardAboutPanel(nil)
         }
 
         Divider()
